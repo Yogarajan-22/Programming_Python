17918513d45756e7a29651c3eca32f5bc6bfecc9
Yogarajan_Linux <yogarajan22@gmail.com>
2021-01-07 20:26:11 +0530

    update

---
+++ test.py
@@ -0,0 +1,20 @@
+print("hello")
+if True:
+    print("tab")
+else:
+    print("false")
+
+def testing(a, b):
+    """add two numbers
+
+    Args:
+        a (int): first value
+        b (int): second value
+
+    Returns:
+        int: sum of two
+    """
+
+    return a + b
+
+v = testing(1, 6)
